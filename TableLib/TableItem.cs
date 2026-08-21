35c6c169f677511634669eec9f7ae008e48b8c29
raco <cekcar@gmail.com>
2026-08-21 22:42:37 +0200

    Table updates, not done

--- TableLib/TableItem.cs
+++ TableLib/TableItem.cs
@@ -1,12 +1,13 @@
 ﻿namespace TableLib
 {
-    public struct TableItem
+    public readonly struct TableItem
     {
         public string Name { get; }
         public string Weight { get; }
         public string Cost { get; }
         public int Rarity { get; }
         public string Description { get; }
+        public string Notes { get; }
         public string Category { get; }
 
         public TableItem(string[] data, string category)
@@ -14,8 +15,14 @@
             Name = data[0];
             Weight = data[1];
             Cost = data[2];
-            Rarity = int.Parse(data[3]);
+
+            if (int.TryParse(data[3].Trim(), out int parsed))
+                Rarity = parsed;
+            else
+                Rarity = 0;
+
             Description = data[4];
+            Notes = data.Length > 5 ? data[5] : string.Empty;
             Category = category;
         }
     }
