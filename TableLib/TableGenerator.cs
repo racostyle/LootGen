004da859f677fdf6a0500cca242e7ea04f516edc
raco <cekcar@gmail.com>
2026-08-25 19:54:21 +0200

    Uniqe store done

--- TableLib/TableGenerator.cs
+++ TableLib/TableGenerator.cs
@@ -30,11 +30,7 @@
                 attempts++;
                 var selectedDataBatch = filtered[Random.Shared.Next(0, filtered.Length)];
 
-                bool isSpotlight = filtered.Length > 1
-                    ? IsTableInSpotlight(selectedDataBatch)
-                    : true;
-
-                if (!isSpotlight)
+                if (!IsInSpotlight(filtered, selectedDataBatch))
                     continue;
 
                 var availableItems = selectedDataBatch.Table.Where(x => x.Rarity <= rarity).ToArray();
@@ -43,9 +39,17 @@
 
 
                 var selected = availableItems[Random.Shared.Next(0, availableItems.Length)];
-
+                
                 if (isUniqueSelection)
                 {
+                    bool hasAllSeedItems = availableItems
+                        .Select(x => x.Hash)
+                        .Distinct()
+                        .All(hash => store.Any(x => x.Hash == hash));
+
+                    if (hasAllSeedItems)
+                        break;
+
                     if (store.Where(x => x.Hash == selected.Hash).Any())
                         continue;
                 }
@@ -58,6 +62,13 @@
                 .OrderBy(x => x.Category)
                 .ThenBy(x => x.Name)
                 .ToArray();
+        }
+
+        private bool IsInSpotlight(IDataBatch[] filtered, IDataBatch selectedDataBatch)
+        {
+            return filtered.Length > 1
+                   ? IsTableInSpotlight(selectedDataBatch)
+                   : true;
         }
 
         private bool IsTableInSpotlight(IDataBatch filtered)
